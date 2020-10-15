f3b3073533e2a81e129dc230c21ea0303761c850
Piergiuseppe Scalamogna <piergiuseppe.scalamogna@gmail.com>
2020-10-15 16:56:14 +0200

    Update Good domain object adding equals, hashcode and copy constructor

--- sources/src/main/java/com/katas/sales_taxes/domain/Good.java
+++ sources/src/main/java/com/katas/sales_taxes/domain/Good.java
@@ -1,11 +1,23 @@
 package com.katas.sales_taxes.domain;
 
+import java.util.Objects;
+
 public class Good {
 
     private String name;
     private boolean taxable;
     private boolean imported;
     private double price;
+
+    public Good() {
+    }
+
+    public Good(Good other) {
+        this.name = other.name;
+        this.taxable = other.taxable;
+        this.imported = other.imported;
+        this.price = other.price;
+    }
 
     public String getName() {
         return name;
@@ -37,5 +49,21 @@ public class Good {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Good good = (Good) o;
+        return taxable == good.taxable &&
+                imported == good.imported &&
+                Double.compare(good.price, price) == 0 &&
+                Objects.equals(name, good.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, taxable, imported, price);
     }
 }

9a2b630b9b905d545223b0a8957294dc3f193c5b
Piergiuseppe Scalamogna <piergiuseppe.scalamogna@gmail.com>
2020-10-15 16:56:06 +0200

    Add Cart domain object

---
+++ sources/src/main/java/com/katas/sales_taxes/domain/Cart.java
@@ -0,0 +1,25 @@
+package com.katas.sales_taxes.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+
+    private Map<Good,Integer> content;
+
+    public Cart() {
+        this.content = new HashMap<>();
+    }
+
+    public Map<Good, Integer> getContent() {
+        return content;
+    }
+
+    public void add(Good good, Integer quantity) {
+        if(content.containsKey(good)){
+            content.put(good, content.get(good) + quantity);
+        } else {
+            content.put(good, quantity);
+        }
+    }
+}
---
+++ sources/src/test/java/com/katas/sales_taxes/domain/CartTest.java
@@ -0,0 +1,31 @@
+package com.katas.sales_taxes.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void add() {
+        Good good = new Good();
+        good.setName("bread");
+        Cart cart = new Cart();
+        cart.add(good, 2);
+
+        assertTrue(cart.getContent().get(good) == 2);
+    }
+
+    @Test
+    void addAnotherOne() {
+        Good good1 = new Good();
+        good1.setName("bread");
+        Good good2 = new Good();
+        good2.setName("bread");
+        Cart cart = new Cart();
+        cart.add(good1, 1);
+        cart.add(good2, 1);
+
+        assertTrue(cart.getContent().get(good1) == 2);
+    }
+}

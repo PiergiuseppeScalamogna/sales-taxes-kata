29082f92753c82eec5a4adeaef72572295da8d24
Piergiuseppe Scalamogna <piergiuseppe.scalamogna@gmail.com>
2020-10-17 10:45:50 +0200

    Refactorize ConsoleReceiptPresenter

--- sources/src/main/java/com/katas/sales_taxes/view/ConsoleReceiptPresenter.java
+++ sources/src/main/java/com/katas/sales_taxes/view/ConsoleReceiptPresenter.java
@@ -8,14 +8,24 @@ public class ConsoleReceiptPresenter implements ReceiptPresenter {
 
     @Override
     public void present(Receipt receipt) {
-        receipt.getPurchases().forEach(purchase -> {
-            System.out.printf(Locale.US, "%d %s%s: %.2f\n",
-                    purchase.getQuantity(),
-                    purchase.getGood().isImported() ? "imported " : "",
-                    purchase.getGood().getName(),
-                    (purchase.getGood().getPrice() * purchase.getQuantity()) + purchase.getTax());
-        });
+        receipt.getPurchases().forEach(this::presentPurchaseDetails);
+        presentTotalTaxes(receipt);
+        presentTotal(receipt);
+    }
+
+    private void presentPurchaseDetails(com.katas.sales_taxes.domain.Purchase purchase) {
+        System.out.printf(Locale.US, "%d %s%s: %.2f\n",
+                purchase.getQuantity(),
+                purchase.getGood().isImported() ? "imported " : "",
+                purchase.getGood().getName(),
+                (purchase.getGood().getPrice() * purchase.getQuantity()) + purchase.getTax());
+    }
+
+    private void presentTotalTaxes(Receipt receipt) {
         System.out.printf(Locale.US, "Sales Taxes: %.2f\n", receipt.getTaxes());
+    }
+
+    private void presentTotal(Receipt receipt) {
         System.out.printf(Locale.US, "Total: %.2f\n", receipt.getTotal());
     }
 }

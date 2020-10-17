b1ff70554886271d745a2c1bde560e636fb82de8
Piergiuseppe Scalamogna <piergiuseppe.scalamogna@gmail.com>
2020-10-17 11:11:07 +0200

    Refactorize DefaultTaxCalculator tests

--- sources/src/test/java/com/katas/sales_taxes/core/DefaultTaxCalculatorTest.java
+++ sources/src/test/java/com/katas/sales_taxes/core/DefaultTaxCalculatorTest.java
@@ -19,41 +19,29 @@ class DefaultTaxCalculatorTest {
 
     @Test
     public void noTaxes() {
-        Good good = new Good();
-        good.setName("bread");
-        good.setImported(false);
-        good.setTaxable(false);
-        good.setPrice(2.5);
+        Good good = new Good("bread", false, false, 2.5);
+
         assertEquals(0.0, taxCalculator.taxOf(good), 0.000001);
     }
 
     @Test
     public void basicTaxes() {
-        Good good = new Good();
-        good.setName("soap");
-        good.setImported(false);
-        good.setTaxable(true);
-        good.setPrice(1.1);
+        Good good = new Good("soap", true, false, 1.1);
+
         assertEquals(0.15, taxCalculator.taxOf(good), 0.000001);
     }
 
     @Test
     public void importTaxes() {
-        Good good = new Good();
-        good.setName("coffee");
-        good.setImported(true);
-        good.setTaxable(false);
-        good.setPrice(3.2);
+        Good good = new Good("coffee", false, true, 3.2);
+
         assertEquals(0.2, taxCalculator.taxOf(good), 0.000001);
     }
 
     @Test
     public void basicAndImportTaxes() {
-        Good good = new Good();
-        good.setName("perfume");
-        good.setImported(true);
-        good.setTaxable(true);
-        good.setPrice(11.75);
+        Good good = new Good("perfume", true, true, 11.75);
+
         assertEquals(1.8, taxCalculator.taxOf(good), 0.000001);
     }
 }
